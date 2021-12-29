2945235c4c112f0185bce6bb24dc141132aa0af0
Maria Ignacia Rodriguez <maria.ignaciarodriguez@live.cl>
2021-12-29 20:55:51 -0300

    Se incorporan nuevas funcionalidades en js y se documentan los archivos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,17 @@
-
+// Al realizar clic mostrará o esconderá la información de las cards
 $('.c-ti').click(function(){
     $('.cards-toggle').slideToggle(400);
   });
+
+window.addEventListener("scroll", function (){
+
+    document.querySelector("nav").classList.toggle("bg-light", window.scrollY < 20);
+    document.querySelector("nav").classList.toggle("bg-black", window.scrollY >=20);
+    
+
+document.querySelector("#btn-up").classList.toggle("d-none", window.scrollY <= 50);
+    })
+    
+document.querySelector("#btn-up").classList.toggle("click", function (){
+    window.scrollTo(0,0);
+    })
