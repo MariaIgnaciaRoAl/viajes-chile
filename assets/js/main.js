ff87ec0f2f88d2e978a74b4f4cc01807b5b11366
Maria Ignacia Rodriguez <maria.ignaciarodriguez@live.cl>
2021-12-29 22:11:11 -0300

    Se incorporan comentarios

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,17 @@ $('.c-ti').click(function(){
     $('.cards-toggle').slideToggle(400);
   });
 
+// Escuchador para el scroll de la ventana
 window.addEventListener("scroll", function (){
 
+// clases del nav
     document.querySelector("nav").classList.toggle("bg-info", window.scrollY < 20);
     document.querySelector("nav").classList.toggle("bg-light", window.scrollY >=20);
-    
-
+ 
 document.querySelector("#btn-up").classList.toggle("d-none", window.scrollY <= 50);
     })
-    
+
+// boton to top
 document.querySelector("#btn-up").addEventListener("click", function (){
     window.scrollTo(0,0);
     })
